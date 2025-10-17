6faee3d1f262711a5e89fa461d1df53ddb18cb45
saberdarkknight <hweiming.chung@gmail.com>
2025-10-18 00:14:18 +0800

    Add tests for add, sub, checksum methods

--- rpc_test.py
+++ rpc_test.py
@@ -1,36 +1,48 @@
 import pytest
 import time
+from multiprocessing import Process
 
 from rpc import RPCClient, RPCServer
-from rpc_server import fibonacci
-
-from multiprocessing import Process
+from rpc_server import add, sub, fibonacci, checksum
 
 @pytest.fixture
 def setup_server():
     server = RPCServer()
+    server.registerMethod(add)
+    server.registerMethod(sub)
     server.registerMethod(fibonacci)
+    server.registerMethod(checksum)
 
     server_process = Process(target=server.run)
     server_process.start()
 
-    time.sleep(1)
+    time.sleep(1)  # wait for server start
 
-    server_address = '127.0.0.1'
-    yield server_address
+    yield '127.0.0.1'
 
     server_process.terminate()
     server_process.join()
 
 
-def test_fibonacci(setup_server):
+def test_add_and_sub(setup_server):
     client = RPCClient(setup_server, 8080)
     client.connect()
     assert client.add(2, 8) == 10
-    assert client.add(3, 9) == 12
     assert client.sub(6, 3) == 3
     client.disconnect()
 
 
+def test_fibonacci(setup_server):
+    client = RPCClient(setup_server, 8080)
+    client.connect()
+    assert client.fibonacci(10) == 55
+    client.disconnect()
+
+
+def test_checksum(setup_server):
+    client = RPCClient(setup_server, 8080)
+    client.connect()
+    assert client.checksum([1, 2, 3, 4]) == 10
+    client.disconnect()
 
     
