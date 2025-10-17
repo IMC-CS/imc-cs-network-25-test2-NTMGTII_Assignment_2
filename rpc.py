571e2b00f6e709e6713b13f6c7155ee92e50eac9
saberdarkknight <hweiming.chung@gmail.com>
2025-10-18 00:11:41 +0800

    Refactor RPC server and client implementation
    Refactor RPCServer and RPCClient classes to improve method registration and connection handling. Added type hints and enhanced error handling.

--- rpc.py
+++ rpc.py
@@ -1,89 +1,148 @@
-import socket
 import json
+import socket
+import inspect
+from threading import Thread
 
+SIZE = 1024
 
 class RPCServer:
-    def __init__(self, host='127.0.0.1', port=8080):
+
+    def __init__(self, host:str='0.0.0.0', port:int=8080) -> None:
         self.host = host
         self.port = port
-        self.methods = {}
+        self.address = (host, port)
+        self._methods = {}
 
-    def registerMethod(self, func):
-        """Register a function that can be called remotely."""
-        self.methods[func.__name__] = func
+    def help(self) -> None:
+        print('REGISTERED METHODS:')
+        for method in self._methods.items():
+            print('\t',method)
 
-    def run(self):
-        """Run the RPC server loop."""
-        print(f"Server running on {self.host}:{self.port}")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((self.host, self.port))
-            s.listen(1)
+    def registerMethod(self, function) -> None:
+        """
+        Pass a method to register all its methods and attributes so they can be used by the client via RPC.
+        Arguments:
+            function -> the method to register
+        """
+        try:
+            self._methods.update({function.__name__ : function})
+        except:
+            raise Exception('A non method object has been passed into RPCServer.registerMethod(self, function)')
+
+    def registerInstance(self, instance=None) -> None:
+        """
+        Pass a instance of a class to register all its methods and attributes so they can be used by the client via RPC.
+        Arguments:
+            instance -> a class object
+        """
+        try:
+            # Regestring the instance's methods
+            for functionName, function in inspect.getmembers(instance, predicate=inspect.ismethod):
+                if not functionName.startswith('__'):
+                    self._methods.update({functionName: function})
+        except:
+            raise Exception('A non class object has been passed into RPCServer.registerInstance(self, instance)')
+
+    def __handle__(self, client:socket.socket, address:tuple):
+        """
+        Pass client connection and its address to perform requests between client and server.
+        Arguments:
+            client -> the socket to use.
+            address -> tuple formed by an ip address and port number
+        """
+        print(f'Managing requests from {address}.')
+        while True:
+            try:
+                functionName, args, kwargs = json.loads(client.recv(SIZE).decode())
+            except: 
+                print(f'! Client {address} disconnected.')
+                break
+            # Showing request Type
+            print(f'{address} : {functionName}({args})')
+            
+            try:
+                response = self._methods[functionName](*args, **kwargs)
+            except Exception as e:
+                # Send back exeption if function called by client is not registred 
+                client.sendall(json.dumps(str(e)).encode())
+            else:
+                client.sendall(json.dumps(response).encode())
+
+
+        print(f'Completed request from {address}.')
+        client.close()
+    
+    def run(self) -> None:
+        """
+        Runs the server.
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            self.socket = sock
+            sock.bind(self.address)
+            sock.listen()
+
+            print('Server running')
             while True:
-                conn, addr = s.accept()
-                print(f"Connection from {addr}")
-                with conn:
-                    data = conn.recv(1024)
-                    if not data:
-                        continue
+                try:
+                    client, address = sock.accept()
 
-                    try:
-                        req = json.loads(data.decode())
-                        method = req.get("method")
-                        params = req.get("params", [])
-                        if method in self.methods:
-                            result = self.methods[method](*params)
-                            resp = {"result": result}
-                        else:
-                            resp = {"error": f"Unknown method '{method}'"}
-                    except Exception as e:
-                        resp = {"error": str(e)}
+                    Thread(target=self.__handle__, args=[client, address]).start()
 
-                    conn.sendall(json.dumps(resp).encode())
+                except KeyboardInterrupt:
+                    print(f'Server {self.address} interrupted')
+                    break
+
+    def shutdown(self):
+        """
+        Stops the server
+        """
+        self.socket.close()
+
 
 
 class RPCClient:
-    def __init__(self, host='127.0.0.1', port=8080):
-        self.host = host
-        self.port = port
-        self.sock = None
+    def __init__(self, host:str='localhost', port:int=8080) -> None:
+        self.__sock = None
+        self.__address = (host, port)
+
+
+    def isConnected(self):
+        try:
+            self.__sock.sendall(b'test')
+            self.__sock.recv(SIZE)
+            return True
+
+        except:
+            return False
+
 
     def connect(self):
-        """Connect to the RPC server."""
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
-
+        try:
+            self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__sock.connect(self.__address)
+        except EOFError as e:
+            print(e)
+            raise Exception('Client was not able to connect.')
+    
     def disconnect(self):
-        """Close the connection."""
-        if self.sock:
-            self.sock.close()
-            self.sock = None
+        try:
+            self.__sock.close()
+        except:
+            pass
 
-    def call(self, method, *params):
-        """Send an RPC request and wait for the result."""
-        if not self.sock:
-            raise ConnectionError("Not connected to server")
 
-        req = {"method": method, "params": params}
-        self.sock.sendall(json.dumps(req).encode())
+    def __getattr__(self, __name: str):
+        def excecute(*args, **kwargs):
+            self.__sock.sendall(json.dumps((__name, args, kwargs)).encode())
 
-        data = self.sock.recv(1024)
-        resp = json.loads(data.decode())
+            response = json.loads(self.__sock.recv(SIZE).decode())
+   
+            return response
+        
+        return excecute
 
-        if "result" in resp:
-            return resp["result"]
-        else:
-            raise Exception(resp.get("error", "Unknown error"))
-
-    # convenience wrappers
-    def add(self, a, b):
-        return self.call("add", a, b)
-
-    def sub(self, a, b):
-        return self.call("sub", a, b)
-
-    def fibonacci(self, n):
-        return self.call("fibonacci", n)
-
-    def checksum(self, byte_array):
-        return self.call("checksum", byte_array)
+    def __del__(self):
+        try:
+            self.__sock.close()
+        except:
+            pass
